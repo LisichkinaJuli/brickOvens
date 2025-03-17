a24d188d6e6d7df6a8f5d0f64265247c05ae33cb
Lisichkina <LisichkinaY@terralink-global.com>
2025-03-17 10:48:10 +0300

    init style directory

---
+++ sd.config.js
@@ -0,0 +1,59 @@
+import { register, permutateThemes } from '@tokens-studio/sd-transforms';
+import StyleDictionary from 'style-dictionary';
+import { readFile, writeFile, mkdir } from 'fs/promises';
+import { dirname } from 'node:path/posix';
+
+register(StyleDictionary, {
+
+  /* options here if needed */
+});
+
+async function run() {
+  console.log('test');
+  const tokens = JSON.parse(await readFile('tokens/tokens.json', 'utf-8'));
+  console.log(tokens);
+  const { $themes, ...sets } = tokens;
+
+  const persistSet = async ([setName, setTokens]) => {
+    const fileName = `tokens/${setName}.json`;
+    const dirName = dirname(fileName);
+    try {
+      await mkdir(dirName, { recursive: true });
+    } catch (e) {
+      // do nothing, dir already exists
+    }
+    await writeFile(fileName, JSON.stringify(setTokens, null, 2), 'utf-8');
+  };
+
+  // persist sets as multi file in tokens folder
+  await Promise.all(Object.entries(sets).map(persistSet));
+
+  const themes = permutateThemes($themes, { separator: '_' });
+  const configs = Object.entries(themes).map(([name, tokensets]) => ({
+    source: Object.keys(sets)
+      .filter(setName => tokensets.includes(setName))
+      .map(setName => `tokens/${setName}.json`),
+    preprocessors: ['tokens-studio'], // <-- since 0.16.0 this must be explicit
+    platforms: {
+      css: {
+        transformGroup: 'tokens-studio',
+        transforms: ['name/kebab'],
+        files: [
+          {
+            destination: `vars-${name}.css`,
+            format: 'css/variables',
+          },
+        ],
+      },
+    },
+  }));
+
+  async function cleanAndBuild(cfg) {
+    const sd = new StyleDictionary(cfg);
+    await sd.cleanAllPlatforms(); // optionally, cleanup files first..
+    await sd.buildAllPlatforms();
+  }
+  await Promise.all(configs.map(cleanAndBuild));
+}
+
+run();
---
+++ src/Grid.jsx
@@ -0,0 +1,15 @@
+import './Grid.css'
+const Grid = () => {
+    return <div className='grid'>
+        <div className="grid__item"></div>
+        <div className="grid__item"></div>
+        <div className="grid__item"></div>
+        <div className="grid__item"></div>
+        <div className="grid__item"></div>
+        <div className="grid__item"></div>
+        <div className="grid__item"></div>
+        <div className="grid__item"></div>
+        <div className="grid__item"></div>
+    </div>
+}
+export default Grid
