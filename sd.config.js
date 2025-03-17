de28a543a6020fd84a9bef58784642880384f56b
Yulia Lisichkina <lisik_ul@mail.ru>
2025-03-17 20:22:20 +0300

    Новая настройка для style dictionary

--- sd.config.js
+++ sd.config.js
@@ -4,18 +4,15 @@ import { readFile, writeFile, mkdir } from 'fs/promises';
 import { dirname } from 'node:path/posix';
 
 register(StyleDictionary, {
-
-  /* options here if needed */
+  
 });
 
 async function run() {
-  console.log('test');
-  const tokens = JSON.parse(await readFile('tokens/tokens.json', 'utf-8'));
-  console.log(tokens);
+  const tokens = JSON.parse(await readFile('tokens.json', 'utf-8'));
   const { $themes, ...sets } = tokens;
 
   const persistSet = async ([setName, setTokens]) => {
-    const fileName = `tokens/${setName}.json`;
+    const fileName = `tokens/build/${setName}.json`;
     const dirName = dirname(fileName);
     try {
       await mkdir(dirName, { recursive: true });
@@ -23,13 +20,14 @@ async function run() {
       // do nothing, dir already exists
     }
     await writeFile(fileName, JSON.stringify(setTokens, null, 2), 'utf-8');
+    
   };
 
   // persist sets as multi file in tokens folder
   await Promise.all(Object.entries(sets).map(persistSet));
 
   const themes = permutateThemes($themes, { separator: '_' });
-  const configs = Object.entries(themes).map(([name, tokensets]) => ({
+  const configs = Object.entries(themes).map(([name, tokensets]) => ({    
     source: Object.keys(sets)
       .filter(setName => tokensets.includes(setName))
       .map(setName => `tokens/${setName}.json`),
@@ -47,8 +45,9 @@ async function run() {
       },
     },
   }));
-
+  console.log('configs');
   async function cleanAndBuild(cfg) {
+    console.log('test');
     const sd = new StyleDictionary(cfg);
     await sd.cleanAllPlatforms(); // optionally, cleanup files first..
     await sd.buildAllPlatforms();
